7cf7bfe52ec7251299021e53c224462306d966cc
dopiehead <ngnimitech@gmail.com>
2024-09-18 16:04:55 +0100

    update

---
+++ subscription.php
@@ -0,0 +1,151 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+<?php include 'components/links.php'; ?>
+    <link rel="stylesheet" href="assets/css/subscription.css">
+    <title>Subscription</title>
+</head>
+<body>
+    <?php include 'components/layout.php'; ?>
+
+     <div class="container mt-10">
+     
+          <div class="subscription-container ">
+
+             <div class="subscription-content">
+             
+              <strong class="text-primary bg-grey text-sm p-2 text-uppercase">
+                basic
+              </strong>
+
+              <p class="text-sm mt-3">For all individuals and starters who
+              want to start with domaining.</p>
+
+              <hr>
+
+              <h5 class="text-dark pricing">N1900</h5>
+              <p class="fw-bold">Per member, per month</p>
+               
+              <div class="d-block">
+              <span><i class="fa fa-check"></i> Access to all features</span>
+              <span><i class="fa fa-check"></i>1k lookups / per month</span>
+              <span><i class="fa fa-close"></i>No API Credits</span>
+              <span><i class="fa fa-check"></i>10 Monitoring Quota</span>
+              <span><i class="fa fa-check"></i>60 minutes Monitoring intervel</span>
+              <span><i class="fa fa-check"></i>20% discount on backorders</span>
+              <span><i class="fa fa-check"></i>Domain Name Appraisal  <b class="coming-soon text-sm">Coming soon</b></span></span>
+              <span><i class="fa fa-check"></i>Ip Monitoring <b class="coming-soon text-sm">Coming soon</b></span></span>
+              <span><i class="fa fa-check"></i>Backlink Monitoring <b class="coming-soon text-sm">Coming soon</b></span></span>
+              </div>
+
+              <a href="" class="btn bg-dark text-white fw-bold form-control mt-3 text-sm">Start free 14-day trial</a>
+             <p class="text-center text-secondary text-sm mt-2">No credit card required</p>
+             </div>
+
+
+
+
+
+
+
+
+
+             <div class="subscription-content bg-danger text-white">
+              
+                   
+             <strong class="text-primary bg-grey text-sm p-2 text-uppercase mb-3">
+               popular
+              </strong>
+            <br><br>
+              <strong class="text-dark bg-white text-sm p-2 text-uppercase mt-5 mb-5">
+               professional
+              </strong><br>
+
+              <p class="mt-3 text-sm">For professional domain names
+              investors with a big portfolio.</p>
+
+              <hr>
+          
+              <h5 style="color:white;" class="pricing text-white">N4900</h5>
+              <p class="fw-bold">Per member, per month</p>
+
+              <div class="d-block">
+              <span><i class="fa fa-check"></i> Access to all features</span>
+              <span><i class="fa fa-check"></i>1k lookups / per month</span>
+              <span><i class="fa fa-check"></i>30k API Credits</span>
+              <span><i class="fa fa-check"></i>10 Monitoring Quota</span>
+              <span><i class="fa fa-check"></i>60 minutes Monitoring intervel</span>
+              <span><i class="fa fa-check"></i>20% discount on backorders</span>
+              <span><i class="fa fa-check"></i>Domain Name Appraisal  <b class="coming-soon text-sm">Coming soon</b></span></span>
+              <span><i class="fa fa-check"></i>Ip Monitoring <b class="coming-soon text-sm">Coming soon</b></span></span>
+              <span><i class="fa fa-check"></i>Backlink Monitoring <b class="coming-soon text-sm">Coming soon</b></span></span>
+              </div>
+
+              <a href="" class="btn btn-warning text-primary fw-bold form-control mt-3">Start free 14-day trial</a>
+             <p class="text-center text-secondary text-sm mt-2">No credit card required</p>
+             </div>
+
+
+
+
+
+
+
+
+
+
+             <div class="subscription-content">
+
+                    
+             <strong class="text-primary bg-grey text-sm p-2 text-uppercase">
+                advanced
+              </strong>
+
+              <p class="text-sm mt-3">For businessess, enterprise domain
+              registrars and registries.</p>
+
+              <hr>
+              <h5 class="text-dark pricing">N9999</h5>
+              <p class="fw-bold">Per member, per month</p>
+
+
+              <div class="d-block">
+              <span><i class="fa fa-check"></i> Access to all features</span>
+              <span><i class="fa fa-check"></i>1k lookups / per month</span>
+              <span><i class="fa fa-check"></i>50k API Credits</span>
+              <span><i class="fa fa-check"></i>10 Monitoring Quota</span>
+              <span><i class="fa fa-check"></i>60 minutes Monitoring intervel</span>
+              <span><i class="fa fa-check"></i>20% discount on backorders</span>
+              <span><i class="fa fa-check"></i>Domain Name Appraisal  <b class="coming-soon text-sm">Coming soon</b></span>
+              <span><i class="fa fa-check"></i>Ip Monitoring <b class="coming-soon text-sm">Coming soon</b></span>
+              <span><i class="fa fa-check"></i>Backlink Monitoring <b class="coming-soon text-sm">Coming soon</b></span>
+              </div>
+
+
+              <a href="" class="btn bg-dark fw-bold form-control text-white mt-3">Start free 14-day trial</a>
+              <p class="text-center text-secondary text-sm mt-2">No credit card required</p>
+
+
+
+
+             
+            </div>
+
+
+ 
+          </div>
+
+
+
+
+
+     </div>
+
+
+
+
+
+    <br><br>
+    <?php include 'components/footer.php'; ?>
+</body>
+</html>
