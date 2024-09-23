18dc33f04221df6bfc6e22f02f32d11bfa74214f
dopiehead <ngnimitech@gmail.com>
2024-09-24 00:18:58 +0100

    new

--- earn.php
+++ earn.php
@@ -14,37 +14,127 @@
 
     <div class="container mt-5">
 
-           <div class="d-flex package-home justify-content-center align-items-center px-3">
+           <div class="package-home px-3">
                
-              <div class="package-container p-2">
+              <div class="package-container">
 
                      <small class="w-100 p-2 bg-skyblue text-center">For Advertsers</small>
 
-                     <h2 class="text-center">Get your prefered audience and results</h2>  
+                     <h3 class="text-center mt-1">Get your prefered audience and results</h3>  
 
-                     <p class="text-sm">Get your prefered audience and results</p>
+                     <p class="text-sm text-center mt-1">Get your prefered audience and results</p>
 
-
-                     <a class="btn btn-info text-white text-center" href="">Get started</a>
-
+                     
+                     <div class="px-2 mt-2 mb-2">
+                     <a class="btn btn-info text-white text-center form-control" href="">Get started</a>
+                     </div>
               </div>
 
 
-              <div class="package-container p-2">
+              <div class="package-container">
 
-                     <small class="w-100 p-2 bg-skyblue text-center">For Earners</small>
+                     <small class="w-100 p-2 bg-skyblue text-center ">For Earners</small>
 
-                     <h2>Earn while Taking Surveys</h2>  
+                     <h2 class="text-center mt-4">Earn while Taking Surveys</h2>  
 
-                     <p class="text-sm">Get your prefered audience and results</p>
+                     <p class="text-sm text-center mt-1">Get your prefered audience and results</p>
                      
-                     <a class="btn btn-info text-white text-center" href="">Get started</a>
-
+                     <div class="px-2 mt-3 mb-2">
+                     <a class="btn btn-info text-white text-center form-control" href="">Get started</a>
+                     </div>
               </div>
 
            </div>
 
 
+      <br><br>
+
+
+        <div class="advert-box">
+
+              <div class="advert-container">
+                   <div class="bg-secondary h-40 w-40"></div>
+                   <div class="d-flex justify-content-space-between"><h6>Areola Ventures</h6>    <span class="text-sm">Technology</span></div>
+                   <strong>N109,150</strong>
+                   <div class="d-flex justify-content-space-between w-100 mt-3"><p class="text-sm">35,300 Likes</p> <p class="text-sm">N10,000 </p></div>
+                   <a href="" class="btn btn-info form-control">Take survey</a>
+              </div>
+
+
+              <div class="advert-container">
+                   <div class="bg-secondary h-40 w-40"></div>
+                   <div><h6>Areola Ventures</h6></div>
+                   <p style="font-size:13px;">Lagos, Nigeria</p>
+                   <div class="d-flex justify-content-space-between mt-2"><p class="text-sm mt-1">35,300 Likes</p> </div>
+                   <a href="" class="btn btn-info form-control">Take survey</a>
+              
+              </div>
+
+
+              <div class="advert-container">
+                   <div class="bg-secondary h-40 w-40"></div>
+                   <div><h6>Areola Ventures</h6></div>
+                 
+                   <div class="d-flex justify-content-space-between mt-5"><p  class="text-sm">35,300 Likes</p> <p class="text-sm">N10,000 </p></div>
+                   <a href="" class="btn btn-info form-control">Take survey</a>
+              
+              </div>
+          
+        
+         </div>
+
+
+
+
+
+         <div class="container row mt-4 mb-5">
+
+             <div class="col-md-6 p-3">
+                
+                  <div class="space">
+
+                  </div>
+
+             </div>
+
+
+             
+             <div class="col-md-6">
+
+                 <h6 class="text-center m-4 fw-bold">What you will Gain</h6>
+
+                 <p class="text-sm">Lorem ipsum dolor sit amet consectetur. Non a nibh convallis gravida tincidunt. Tellus nisi malesuada elementum fermentum magnis massa netus a elementum. Est adipiscing vulputate sit fermentum et sit. Id quam leo morbi facilisis lectus mi mi volutpat. Magnis morbi dui feugiat volutpat leo. Mauris feugiat eu velit amet. Tellus lectus velit feugiat morbi congue tristique elit. Malesuada curabitur pellentesque dictum a enim. Nulla elit sit neque sagittis amet nulla dis non. Nascetur facilisis mauris ornare enim.
+                 Sem eu urna est amet tincidunt. Nibh mauris tortor egestas amet justo consequat lacus lectus proin. Tempor enim nibh id vel egestas morbi. Mauris nam nulla sed sed semper a mauris duis nulla. Nunc gravida nulla elementum dolor consectetur pellentesque suspendisse ut tempor.</p>
+                
+                 <div class="gain_row">
+
+                     <div>
+                        <div class="bg-secondary my-2 rounded"></div>
+                        <h6 class="text-center"><b>Lorem</b></h6>
+                     </div>
+
+
+
+                     <div>
+                        <div class="bg-secondary my-2 rounded"></div>
+                        <h6 class="text-center"><b>Lorem</b></h6>
+                     </div>
+
+
+
+                     <div>
+                        <div class="bg-secondary my-2 rounded"></div>
+                        <h6 class="text-center"><b>Lorem</b></h6>
+                     </div>
+
+
+                 </div>
+                 
+
+
+             </div>
+
+         </div>
 
 
     </div>
